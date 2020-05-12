506657ea965c0590ca8151d62e5417fab2ebc60c
William Rogge <williamroggeiv@gmail.com>
2020-05-12 11:40:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.size == 0
-    katz_deli.push("#{name}")
+    katz_deli.push(name)
     katz_deli.join(" ")
     puts "Welcome, #{name}. You are number 1 in line."
     
