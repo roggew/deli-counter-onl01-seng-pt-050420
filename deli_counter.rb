4d273c77aa1772a0b0eee6d9a5195e8e2ebe4528
William Rogge <williamroggeiv@gmail.com>
2020-05-12 12:07:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -31,6 +31,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.size == 0
      puts "There is nobody waiting to be served!" 
+   
      elsif katz_deli.size > 1
      puts "Now serving #{katz_deli}."
      katz_deli.shift
